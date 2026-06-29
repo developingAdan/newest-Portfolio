c60c1bc040df50e1758522e000f67c03fb980d3b
Adan Garcia <developingadan@gmail.com>
2026-06-29 03:02:30 -0500

    updating proj section

--- forms/contact.php
+++ forms/contact.php
@@ -17,11 +17,30 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
+  // Validate required fields
+  $name  = isset($_POST['name'])         ? trim($_POST['name'])         : '';
+  $email = isset($_POST['email'])        ? trim($_POST['email'])        : '';
+  $phone = isset($_POST['Phone Number']) ? trim($_POST['Phone Number']) : '';
+  $msg   = isset($_POST['message'])      ? trim($_POST['message'])      : '';
+
+  if (empty($name) || empty($email) || empty($msg)) {
+    die('Please fill in all required fields.');
+  }
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    die('Invalid email address.');
+  }
+
+  // Strip any newlines from fields used in headers to prevent injection
+  $name  = str_replace(["\r", "\n"], '', $name);
+  $email = str_replace(["\r", "\n"], '', $email);
+  $phone = str_replace(["\r", "\n"], '', $phone);
+
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['Phone Number'];
+  $contact->from_name = $name;
+  $contact->from_email = $email;
+  $contact->subject = $phone;
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,9 +52,9 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $name,  'From');
+  $contact->add_message( $email, 'Email');
+  $contact->add_message( $msg,   'Message', 10);
 
   echo $contact->send();
 ?>
